4df93128086650f056a3ec730717ad7a3ddf712a
Koganas <danielsank@gmail.com>
2021-02-07 11:03:38 -0300

    add index page template; introduce tailwind.css (base, components and utilities) with custom button; sass support;

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<div
+		class="flex justify-center w-full min-h-screen py-24 font-sans bg-gray-200"
+	>
+		<!-- TODO: AlertStatus -->
+
+		<div class="flex flex-col w-80 items-center">
+			<h1 class="mb-2 text-6xl font-light text-greenTurq">Offices</h1>
+
+			<!-- TODO: OfficeNew and OfficeCard -->
+
+			<footer class="mt-6 text-center">
+				<p class="text-gray-500 font-light">
+					This project is for test purpose only.
+				</p>
+				<p>
+					<a
+						class="uppercase text-greenTurq text-xs opacity-75"
+						href="https://www.dogandponystudios.com/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						www.dogandponystudio.com
+					</a>
+				</p>
+			</footer>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+	layout: 'default',
+	data: () => {
+		return {}
+	},
+	computed: {
+	},
+	head() {
+		return {
+			title: 'Offices - Dog and Pony Test'
+		}
+	}
+}
+</script>
